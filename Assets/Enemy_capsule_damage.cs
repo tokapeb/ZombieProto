3e79b3bb8a11b244b3e87b2a98607ae3234cd6d8
tokapeb <tokap@gmail.com>
2015-04-12 19:19:11 +0200

    Bad solution to distance keeping and following, have to try something else.

--- Assets/Enemy_capsule_damage.cs
+++ Assets/Enemy_capsule_damage.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class Enemy_capsule_damage : MonoBehaviour {
 
 	public float enemyRotationSpeed;
+	public float desiredDistance;
 	public Transform target;
+	public float enemySpeed;
 
 	private Quaternion _lookRotation;
 	private Vector3 _direction;
@@ -21,28 +23,42 @@ public class Enemy_capsule_damage : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-//		this.gameObject.GetComponent<Renderer> ().material.color = Color.red;
 
 		enemyRotationSpeed = 6f;
+
+		desiredDistance = 1.7f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		// Player detection part.
 		RaycastHit hit;
 		CapsuleCollider cpasCol = GetComponent<CapsuleCollider>();
 //		CharacterController charCtrl = GetComponent<CharacterController>();
 		Vector3 p1 = transform.position + cpasCol.center;
 		if (Physics.SphereCast (p1, cpasCol.height / 2, transform.forward, out hit, 2)) {
 			float distanceToObstacle = hit.distance;
-			if (hit.collider.tag != "Enemy") {
-				Debug.Log(this.name + " detects: " + hit.collider.tag);
+			if (hit.collider.tag == "Player") {
+//				Debug.Log(this.name + " detects: " + hit.collider.tag);
+				// Turn to player part.
 				target = hit.transform;
 
 				_direction = (target.position - transform.position).normalized;
 				_lookRotation = Quaternion.LookRotation(_direction);
 
 				transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * enemyRotationSpeed);
+
+				// Follow part.
+				Vector3 Diff = transform.position - target.position;
+//				if (Diff.magnitude > desiredDistance) {
+					float mult = desiredDistance / Diff.magnitude;
+
+					transform.position -= new Vector3 (Diff.x, 0f, Diff.z);
+
+					transform.position += new Vector3 (Diff.x, 0f, Diff.z) * mult;
+//				}
+
 
 			}
 		}
